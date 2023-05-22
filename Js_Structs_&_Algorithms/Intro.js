fc40abb1e6c25c2de1bb7d6cc8fe576d9fcf6d7e
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-22 17:43:03 +0100

    Update Intro.js

--- Js_Structs_&_Algorithms/Intro.js
+++ Js_Structs_&_Algorithms/Intro.js
@@ -1,9 +1,10 @@
-/*
+/* pamjit manager
 //Data types and declarations :
 var = Variable
 let = can be used to name a variable the same name as a previous var declaration
 const = read only const
 
+( % ) = remainder equals the remainder of a division not a result
 
 
 */
