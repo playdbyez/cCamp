0be2d5874e758ae5a0aa30857c2a0d963d0bf396
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-23 11:47:42 +0100

    Update Intro.js

--- Js_Structs_&_Algorithms/Intro.js
+++ Js_Structs_&_Algorithms/Intro.js
@@ -1,4 +1,5 @@
 /* pamjit manager
+
 //Data types and declarations :
 var = Variable
 let = can be used to name a variable the same name as a previous var declaration
@@ -7,4 +8,16 @@ const = read only const
 ( % ) = remainder equals the remainder of a division not a result
 
 ( ' ) / ( " ) = single or double quotes make no diff
+
+
+
+//Functions
+console.log(""); = basic output function
+
+console.log("text".length); = size of string output
+
+variable.length = size of variable, not  function, braces are not required
+
+( [] ) = Subscript operator works the same
+
 */
