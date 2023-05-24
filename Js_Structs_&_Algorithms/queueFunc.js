d44e5d87f587c48e7200fca87a0fd2d54df90e29
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-24 13:20:19 +0100

    Update queueFunc.js

--- Js_Structs_&_Algorithms/queueFunc.js
+++ Js_Structs_&_Algorithms/queueFunc.js
@@ -12,3 +12,6 @@ let testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+// Output
+2, 3, 4, 5, 6
