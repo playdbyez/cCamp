77a3d648705f340c9970c39347e181935da47e99
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-25 17:42:37 +0100

    Update converType.js

--- Js_Structs_&_Algorithms/converType.js
+++ Js_Structs_&_Algorithms/converType.js
@@ -4,4 +4,4 @@ console.log(parseInt("57"));
 // ParseInt's second argument is a radix that specifies the base of number used for conversion
 //The radix can be an integer between 2 and 36.
 
-//Ternary operator (?) exists in Js
+
