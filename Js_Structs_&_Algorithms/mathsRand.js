3ad7167b5cdc55385bce9eb0f23e51136598f60c
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-25 17:14:43 +0100

    Update and rename randFract.js to mathsRand.js

---
+++ Js_Structs_&_Algorithms/mathsRand.js
@@ -0,0 +1,15 @@
+
+
+// The function Math.random generates a decimal number from bellow 1 however completely random
+
+return Math.random();
+// Output
+0.7678654445239281
+
+
+// The function Math.floor will truncate the result into an integer type of result
+
+var numb = 1.8678654445239281;
+return Math.floor(numb);
+// Output
+0
--- Js_Structs_&_Algorithms/randFract.js
+++
@@ -1,5 +0,0 @@
-// The function Math.random generates a decimal number from bellow 1 however completely random
-
-return Math.random();
-// Output
-0.7678654445239281
