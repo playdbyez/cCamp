668c35ffcf62edf4697d2615b4f43d242cf89b71
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-25 17:18:41 +0100

    Update mathsRand.js

--- Js_Structs_&_Algorithms/mathsRand.js
+++ Js_Structs_&_Algorithms/mathsRand.js
@@ -13,3 +13,9 @@ var numb = 1.8678654445239281;
 return Math.floor(numb);
 // Output
 0
+
+
+//However combined both functions can generate an integer withouth any values hard-coded
+console.log(Math.floor(Math.random() * 10));
+// Output
+random int from 1 - 9 
