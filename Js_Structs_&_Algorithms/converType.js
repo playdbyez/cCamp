d12820a065ea3eb68e57d6225f3bbb5d9ae8f88c
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-25 17:40:20 +0100

    Create converType.js

---
+++ Js_Structs_&_Algorithms/converType.js
@@ -0,0 +1,7 @@
+//string to int
+console.log(parseInt("57"));
+
+// ParseInt's second argument is a radix that specifies the base of number used for conversion
+//The radix can be an integer between 2 and 36.
+
+//Ternary operator (?) exists in Js
