a17502e06d24034482368e6a71ba4f90000ca715
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-25 13:20:32 +0100

    Create nestingFor.js

---
+++ Js_Structs_&_Algorithms/nestingFor.js
@@ -0,0 +1,10 @@
+//Its easy to operate on multiple arrays
+
+const arr = [[1, 2], [3, 4], [5, 6, 7]];
+var product =1;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {product *=arr[i][j];
+    
+  }
+}
+console.log(product);
