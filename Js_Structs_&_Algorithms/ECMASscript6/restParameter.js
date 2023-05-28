0abd8de73cb95f01e7b075b58d0ef09debf893e1
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-28 12:17:33 +0100

    Update restParameter.js

--- Js_Structs_&_Algorithms/ECMASscript6/restParameter.js
+++ Js_Structs_&_Algorithms/ECMASscript6/restParameter.js
@@ -1,2 +1,14 @@
 //The rest parameter allows an indefinite number of arguments to be passed to a function at the same time.
 (...)
+ 
+ 
+ //USAGE
+ //Instead of one parameter, the write arrow function, sum now takes any number of arguments thanks to the rest parameter(...args)
+ const sum = (...args) => {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {total += args[i];}
+  return total;
+                                }
+
+
+
