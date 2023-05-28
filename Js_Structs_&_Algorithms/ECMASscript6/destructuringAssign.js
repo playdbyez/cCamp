96cb0fc3ab98b1fa3f4baa898672bf648b6fa115
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-05-28 12:46:52 +0100

    Update destructuringAssign.js

--- Js_Structs_&_Algorithms/ECMASscript6/destructuringAssign.js
+++ Js_Structs_&_Algorithms/ECMASscript6/destructuringAssign.js
@@ -1,4 +1,5 @@
-//This assignment makes the declaration of functions easier 
+//This assignment makes the declaration parameter calls much easier 
+//By grouping together the assignment of parameters of a function that matches the variables pre-assigned in the declaration of the Object.
 
 //BEFORE
 const user = { name: 'John Doe', age: 34 };
@@ -9,3 +10,5 @@ const age = user.age;
 
 //AFTER
 const { name, age } = user;
+
+
