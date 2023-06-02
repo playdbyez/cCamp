c7a8b44ecbae2e1ba837ac0db74f2000c845c480
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-06-02 16:37:38 +0100

    Update destructuringAssign.js

--- Js_Structs_&_Algorithms/ECMASscript6/destructuringAssign.js
+++ Js_Structs_&_Algorithms/ECMASscript6/destructuringAssign.js
@@ -31,3 +31,19 @@ console.log(userName);
 // Output
 John Doe
 John Doe
+
+
+//FURTHER
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+  
+const  {today : {low: lowToday}} = LOCAL_FORECAST;
+const  {today : {high: highToday}} = LOCAL_FORECAST;
+
+//> Declare type > Open braces for a nested object > Colon 
+//> Open braces for nested within nested parameter > the last name will be the new name to this index
+//> Close parenthesis and assigned this variable to the object it is indexing.
